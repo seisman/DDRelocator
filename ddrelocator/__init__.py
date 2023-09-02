f010bf5a4447d131ac601818664f7e59fe1ab33d
Dongdong Tian <seisman.info@gmail.com>
2023-09-02 20:39:02 +0800

    Update docstrings in __init__.py

--- ddrelocator/__init__.py
+++ ddrelocator/__init__.py
@@ -1,5 +1,9 @@
 """
 ddrelocator
+===========
+
+A Python package for determining the relative location of two nearby earthquakes
+using the master-event algorithm.
 """
 from ddrelocator.headers import Event, Obs, SearchParams, Solution, Station
 from ddrelocator.locator import find_best_solution, gridsearch, try_solution
