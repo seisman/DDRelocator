c9dbbb142fe37fbbd94791cc7f7ad3ac43924bcb
Dongdong Tian <seisman.info@gmail.com>
2023-12-03 15:26:28 +0800

    Update run_ddrelocator.py

--- examples/ex1/run_ddrelocator.py
+++ examples/ex1/run_ddrelocator.py
@@ -12,8 +12,8 @@ master = Event("2018-02-01T00:00:00", 36.1688, 139.8075, 53.45, 4.7)
 # Information of the slave event [unknown]
 slave = Event("2018-02-02T00:00:00", 36.1678, 139.8095, 53.45, 4.7)
 
-# read observations from a file
-obslist = read_obslist("obs.dat")
+# observation file
+obsfile = "obs.dat"
 
 # search parameters
 params = SearchParams(
@@ -26,6 +26,10 @@ print("Ex1 for ddrelocator")
 print("Master event: ", master)
 print("Slave event: ", slave)
 
+# read observations from a file
+obslist = read_obslist(obsfile)
+print(f"Read {len(obslist)} observations from {obsfile}")
+
 # visualize the observations
 plot_dt(obslist, master, show_unused=True)
 
@@ -35,7 +39,7 @@ start = time.time()
 sol, grid, Jout = gridsearch(master, obslist, params)
 print(f"Done in {time.time() - start:.1f} sec")
 
-# Try the best solution again to add more properties like tmean
+# Try the best solution again to add more properties like tmean and residuals
 try_solution(obslist, sol, keep_residual=True)
 # the best location for the slave event
 slave_sol = Event(
--- examples/ex2/run_ddrelocator.py
+++ examples/ex2/run_ddrelocator.py
@@ -12,8 +12,8 @@ master = Event("2003-07-02T00:47:11.860", -3.643, 102.060, 75.2, 5.1)
 # Information of the slave event [to be relocated]
 slave = Event("1995-11-14T06:32:55.750", -3.682, 101.924, 57.0, 5.1)
 
-# read observations from a file
-obslist = read_obslist("obs-2003-1995.dat")
+# observation file
+obsfile = "obs-2003-1995.dat"
 
 # search parameters
 params = SearchParams(
@@ -26,6 +26,10 @@ print("Ex2 for ddrelocator")
 print("Master event: ", master)
 print("Slave event: ", slave)
 
+# read observations from a file
+obslist = read_obslist(obsfile)
+print(f"Read {len(obslist)} observations from {obsfile}")
+
 # visualize the observations
 plot_dt(obslist, master, show_unused=True)
 
@@ -35,13 +39,12 @@ start = time.time()
 sol, grid, Jout = gridsearch(master, obslist, params)
 print(f"Done in {time.time() - start:.1f} sec")
 
-# Try the best solution again to add more properties like tmean
+# Try the best solution again to add more properties like tmean and residuals
 try_solution(obslist, sol, keep_residual=True)
 # the best location for the slave event
 slave_sol = Event(
     slave.origin + sol.tmean, sol.latitude, sol.longitude, sol.depth, slave.magnitude
 )
-
 print(
     "Best solution:\n"
     f"dlat: {sol.dlat:.5f}\n"
