a2083b39688bd57653d05d4f24fcd51b3fd98016
Dongdong Tian <seisman.info@gmail.com>
2024-04-03 14:57:08 +0800

    Refactor classes in headers.py using dataclass

--- ddrelocator/headers.py
+++ ddrelocator/headers.py
@@ -2,40 +2,44 @@
 Classes for ddrelocator.
 """
 
+from dataclasses import dataclass
+
 import numpy as np
 from obspy import UTCDateTime
 
 
+@dataclass
 class Event:
     """
     Class for event information.
+
+    Parameters
+    ----------
+    origin
+        Event origin time.
+    latitude
+        Event latitude.
+    longitude
+        Event longitude.
+    depth
+        Event depth in km.
+    magnitude
+        Event magnitude (unused).
+
+    Attributes
+    ----------
+    id : str
+        Event ID in the format of YYYYMMDDHHMMSS.
     """
 
-    def __init__(self, origin, latitude, longitude, depth, magnitude=None):
-        """
-        Parameters
-        ----------
-        origin : str
-            Event origin time.
-        latitude : float
-            Event latitude.
-        longitude : float
-            Event longitude.
-        depth : float
-            Event depth in km.
-        magnitude : float
-            Event magnitude (unused).
+    origin: str | UTCDateTime
+    latitude: float
+    longitude: float
+    depth: float
+    magnitude: float
 
-        Attributes
-        ----------
-        id : str
-            Event ID in the format of YYYYMMDDHHMMSS.
-        """
-        self.origin = UTCDateTime(origin)
-        self.latitude = latitude
-        self.longitude = longitude
-        self.depth = depth  # in km
-        self.magnitude = magnitude  # unused
+    def __post_init__(self):
+        self.origin = UTCDateTime(self.origin)
         self.id = self.origin.strftime("%Y%m%d%H%M%S")
 
     def __str__(self):
@@ -44,93 +48,78 @@ class Event:
         )
 
 
+@dataclass
 class Station:
     """
     Class for station information.
+
+    Parameters
+    ----------
+    name
+        Station name.
+    latitude
+        Station latitude.
+    longitude
+        Station longitude.
+    elevation
+        Station elevation (unused).
     """
 
-    def __init__(self, name, latitude, longitude, elevation=0.0):
-        """
-        Parameters
-        ----------
-        name : str
-            Station name.
-        latitude : float
-            Station latitude.
-        longitude : float
-            Station longitude.
-        elevation : float
-            Station elevation (unused).
-        """
-        self.name = name
-        self.latitude = latitude
-        self.longitude = longitude
-        self.elevation = elevation  # unused
+    name: str
+    latitude: float
+    longitude: float
+    elevation: float = 0.0  # unused
 
 
+@dataclass
 class Obs:
     """
     Class for observations.
+
+    Parameters
+    ----------
+    station
+        Station name.
+    latitude
+        Station latitude.
+    longitude
+        Station longitude.
+    distance
+        Distance from master event to station in degrees.
+    azimuth
+        Azimuth from master event to station in degrees.
+    phase
+        Phase name.
+    time
+        Predicted travel time in second.
+    dtdd
+        Horizontal slowness in s/deg.
+    dtdh
+        Vertical slowness in s/km.
+    dt
+        Travel time difference.
+    weight
+        Weight for the observation.
+
+    Attributes
+    ----------
+    dt_pre : float
+        Predicted travel time difference for a given solution.
+    residual : float
+        Travel time difference residual for a given solution.
     """
 
-    def __init__(
-        self,
-        station,
-        latitude,
-        longitude,
-        distance,
-        azimuth,
-        phase,
-        time,
-        dtdd,
-        dtdh,
-        dt,
-        weight=1.0,
-    ):
-        """
-        Parameters
-        ----------
-        station : str
-            Station name.
-        latitude : float
-            Station latitude.
-        longitude : float
-            Station longitude.
-        distance : float
-            Distance from master event to station in degrees.
-        azimuth : float
-            Azimuth from master event to station in degrees.
-        phase : str
-            Phase name.
-        time : float
-            Predicted travel time in second.
-        dtdd : float
-            Horizontal slowness in s/deg.
-        dtdh : float
-            Vertical slowness in s/km.
-        dt : float
-            Travel time difference.
-        weight : float
-            Weight for the observation.
-
-        Attributes
-        ----------
-        dt_pre : float
-            Predicted travel time difference for a given solution.
-        residual : float
-            Travel time difference residual for a given solution.
-        """
-        self.station = station
-        self.latitude = latitude
-        self.longitude = longitude
-        self.distance = distance
-        self.azimuth = azimuth
-        self.phase = phase
-        self.time = time
-        self.dtdd = dtdd
-        self.dtdh = dtdh
-        self.dt = dt
-        self.weight = weight
+    station: str
+    latitude: float
+    longitude: float
+    distance: float
+    azimuth: float
+    phase: str
+    time: float
+    dtdd: float
+    dtdh: float
+    dt: float
+    weight: float = 1.0
 
 
 class Solution:
@@ -159,18 +148,20 @@ class Solution:
             RMS of travel time residuals.
         """
         self.type = type
-        if self.type == "geographic":
-            self.dlat, self.dlon, self.ddepth = params
-        elif self.type == "cylindrical":
-            self.ddist, self.az, self.ddepth = params
-        else:
-            raise ValueError("Unrecognized solution type '{type}'.")
+        match self.type:
+            case "geographic":
+                self.dlat, self.dlon, self.ddepth = params
+            case "cylindrical":
+                self.ddist, self.az, self.ddepth = params
+            case _:
+                raise ValueError(f"Unrecognized solution type '{type}'.")
 
     def __str__(self):
-        if self.type == "geographic":
-            result = [f"dlat: {self.dlat}°", f"dlon: {self.dlon}°"]
-        elif self.type == "cylindrical":
-            result = [f"ddist: {self.ddist} m", f"az: {self.az}°"]
+        match self.type:
+            case "geographic":
+                result = [f"dlat: {self.dlat}°", f"dlon: {self.dlon}°"]
+            case "cylindrical":
+                result = [f"ddist: {self.ddist} m", f"az: {self.az}°"]
 
         result.append(f"ddepth: {self.ddepth} km")
         for attr in ["tmean", "misfit"]:
@@ -191,19 +182,20 @@ class Solution:
         """
         origin = (master.origin if slave is None else slave.origin) + self.tmean
         magnitude = master.magnitude if slave is None else slave.magnitude
-        if self.type == "geographic":
-            latitude = master.latitude + self.dlat
-            longitude = master.longitude + self.dlon
-            depth = master.depth + self.ddepth
-        elif self.type == "cylindrical":
-            earth_radius = 6371.0  # km
-            ddist = self.ddist / 1000.0  # convert distance from meter to km
-            azimuth = np.radians(self.az)  # convert azimuth from degree to radian
-            latitude, longitude = np.radians([master.latitude, master.longitude])
-            latitude += ddist / earth_radius * np.cos(azimuth)
-            longitude += ddist / earth_radius * np.sin(azimuth) / np.cos(latitude)
-            latitude, longitude = np.degrees([latitude, longitude])
-            depth = master.depth + self.ddepth / 1000.0
+        match self.type:
+            case "geographic":
+                latitude = master.latitude + self.dlat
+                longitude = master.longitude + self.dlon
+                depth = master.depth + self.ddepth
+            case "cylindrical":
+                earth_radius = 6371.0  # km
+                ddist = self.ddist / 1000.0  # convert distance from meter to km
+                azimuth = np.radians(self.az)  # convert azimuth from degree to radian
+                latitude, longitude = np.radians([master.latitude, master.longitude])
+                latitude += ddist / earth_radius * np.cos(azimuth)
+                longitude += ddist / earth_radius * np.sin(azimuth) / np.cos(latitude)
+                latitude, longitude = np.degrees([latitude, longitude])
+                depth = master.depth + self.ddepth / 1000.0
 
         return Event(
             origin=origin,
