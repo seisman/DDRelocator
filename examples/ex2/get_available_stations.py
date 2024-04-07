ded457808c52793ef6d49181194080b3444f5cef
Dongdong Tian <seisman.info@gmail.com>
2024-04-07 15:17:32 +0800

    Update to download all broadband channels

--- examples/ex2/get_available_stations.py
+++ examples/ex2/get_available_stations.py
@@ -8,7 +8,7 @@ client = RoutingClient("iris-federator")
 inv = client.get_stations(
     starttime="1995-11-14T00:00:00.000",
     endtime="1995-11-14T23:59:59.999",
-    channel="BHZ",
+    channel="BH?",
     level="station",
 )
 inv.write("1995-stations.xml", format="stationxml")
